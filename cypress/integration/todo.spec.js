16084fa9dbc5c0de25d86c31aa961003e7feed18
vladik9 <v.cornici@gmail.com>
2021-08-16 15:07:19 +0300

    test connectoin

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -1,16 +1,14 @@
-/// <reference types="cypress" />
+//this thing block my page
+// <reference types="cypress" />
 
-describe('a test scenario', () => {
+describe("a test scenario", () => {
   beforeEach(() => {
+    cy.visit("http://live.demoguru99.com");
+  });
 
-    cy.visit('http://live.demoguru99.com')
-
-  })
-
-  it('will contain a test case', () => {
-   cy.get('body div.header-language-background p')
-       .should('be.visible')
-       .contains('Default welcome msg!')
-  })
-
-})
+  it("will contain a test case", () => {
+    cy.get("body div.header-language-background p")
+      .should("be.visible")
+      .contains("Default welcome msg!");
+  });
+});
