214fe036077314ed2ae5c9692f556ffdd655854b
vladik9 <v.cornici@gmail.com>
2021-08-17 15:04:51 +0300

    second task

--- cypress/integration/Task2.spec.js
+++ cypress/integration/Task2.spec.js
@@ -1,7 +1,7 @@
 var password = "amParolaCea1MaiTare"; // amParolaCea1MaiTare
 var email = "canomeI105@email.com"; //canomeI105@email.com
 
-describe("a test scenario", () => {
+describe("Test search funtionality", () => {
   beforeEach(() => {
     cy.visit("http://live.demoguru99.com");
   });
@@ -31,7 +31,44 @@ describe("a test scenario", () => {
   //   cy.get("#header-account > div > ul > li.last > a").click();
   // });
 
-  it("Test add an item to shoping cart", () => {
+  // it.only("Test add an item to shoping cart", () => {
+  //   cy.get("#header > div > div.skip-links > div > a")
+  //     .should("have.class", "skip-link skip-account")
+  //     .click();
+  //   cy.get("#header-account > div > ul > li.last > a")
+  //     .should("contain", "Log In")
+  //     .click();
+  //   cy.get("#email")
+  //     .should("have.id", "email")
+  //     .type(email)
+  //     .should("have.value", email);
+  //   cy.get("#pass")
+  //     .should("have.class", "input-text required-entry validate-password")
+  //     .type(password)
+  //     .should("have.value", password);
+  //   cy.get("#send2").should("contain", "Login").click();
+
+  //   cy.get(".nav-1 > .level0").should("have.class", "level0").click();
+  //   cy.get("#search").click().type("IPHONE").clear().type("IPHONE 6");
+
+  //   cy.get("#search_autocomplete > ul").first().click();
+
+  //   cy.get(":nth-child(2) > .product-info > .actions > .button").click();
+
+  //   cy.get("li > span").should(
+  //     "have.text",
+  //     "IPhone was added to your shopping cart."
+  //   );
+  //   cy.get(".a-center > .btn-remove").click();
+  //   cy.get("h1").should("have.text", "Shopping Cart is Empty");
+
+  //   cy.get("#header > div > div.skip-links > div > a")
+  //     .should("have.class", "skip-link skip-account")
+  //     .click();
+  //   cy.get("#header-account > div > ul > li.last > a").click();
+  // });
+
+  it.only("Test cypress to finalize a purchase", () => {
     cy.get("#header > div > div.skip-links > div > a")
       .should("have.class", "skip-link skip-account")
       .click();
@@ -52,14 +89,20 @@ describe("a test scenario", () => {
     cy.get("#search").click().type("IPHONE").clear().type("IPHONE 6");
 
     cy.get("#search_autocomplete > ul").first().click();
-    cy.get(".button btn-cart").click();
+
+    cy.get(":nth-child(2) > .product-info > .actions > .button").click();
+
+    cy.get("li > span").should(
+      "have.text",
+      "IPhone was added to your shopping cart."
+    );
+    cy.get(".method-checkout-cart-methods-onepage-bottom").click();
+
+    cy.get("input-box").should("have.id", "company").type("checkbox");
 
     // cy.get("#header > div > div.skip-links > div > a")
     //   .should("have.class", "skip-link skip-account")
     //   .click();
     // cy.get("#header-account > div > ul > li.last > a").click();
   });
-
-  // it("will contain a test case", () => {
-  // });
 });
