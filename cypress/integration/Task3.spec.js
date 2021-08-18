a51cf4122819d2258183a7b1df6133794e77bbf2
vladik9 <v.cornici@gmail.com>
2021-08-18 10:06:51 +0300

    finish 3 task

--- cypress/integration/Task3.spec.js
+++ cypress/integration/Task3.spec.js
@@ -6,7 +6,7 @@ describe("Test funtionality for registred user", () => {
     cy.visit("http://live.demoguru99.com");
   });
 
-  it("Test search an item", () => {
+  it("Test update an account information ", () => {
     cy.get("#header > div > div.skip-links > div > a")
       .should("have.class", "skip-link skip-account")
       .click();
@@ -22,17 +22,72 @@ describe("Test funtionality for registred user", () => {
       .type(password)
       .should("have.value", password);
     cy.get("#send2").should("contain", "Login").click();
-
-    cy.get(".nav-1 > .level0").should("have.class", "level0").click();
-    cy.get("#search").click().type("IPHONE").clear().type("IPHONE 6");
-    cy.get("h1").should("have.text", "Mobile");
+    cy.get("h1").should("have.text", "My Dashboard");
+    cy.get("a")
+      .contains("Account Information")
+      .should("have.html", "Account Information")
+      .click();
+    cy.get("#firstname")
+      .should("have.value", "Camoe")
+      .clear()
+      .type("Camoe")
+      .should("have.value", "Camoe");
+    cy.get("#current_password")
+      .should("have.value", "")
+      .type(password)
+      .should("have.value", password);
+    cy.get("button").contains("Save").click();
+    cy.get("li > span").should(
+      "have.text",
+      "The account information has been saved."
+    );
     cy.get("#header > div > div.skip-links > div > a")
       .should("have.class", "skip-link skip-account")
       .click();
     cy.get("#header-account > div > ul > li.last > a").click();
   });
 
-  it("Test add an item to shoping cart", () => {
+  it("Test adding new address ", () => {
+    cy.get("#header > div > div.skip-links > div > a")
+      .should("have.class", "skip-link skip-account")
+      .click();
+    cy.get("#header-account > div > ul > li.last > a")
+      .should("contain", "Log In")
+      .click();
+    cy.get("#email")
+      .should("have.id", "email")
+      .type(email)
+      .should("have.value", email);
+    cy.get("#pass")
+      .should("have.class", "input-text required-entry validate-password")
+      .type(password)
+      .should("have.value", password);
+    cy.get("#send2").should("contain", "Login").click();
+    cy.get("h1").should("have.text", "My Dashboard");
+    cy.get("a")
+      .contains("Address Book")
+      .should("have.html", "Address Book")
+      .click();
+    cy.get("a")
+      .contains("Change Billing Address")
+      .should("have.text", "Change Billing Address")
+      .click();
+    cy.get("#street_2")
+      .should("have.class", "input-text")
+      .clear()
+      .type("the new way nr.2")
+      .should("have.value", "the new way nr.2");
+    cy.get("button").contains("Save Address").click();
+    cy.get("span")
+      .contains("The address has been saved.")
+      .should("have.text", "The address has been saved.");
+    cy.get("#header > div > div.skip-links > div > a")
+      .should("have.class", "skip-link skip-account")
+      .click();
+    cy.get("#header-account > div > ul > li.last > a").click();
+  });
+
+  it("Test for wishlist ", () => {
     cy.get("#header > div > div.skip-links > div > a")
       .should("have.class", "skip-link skip-account")
       .click();
@@ -51,48 +106,54 @@ describe("Test funtionality for registred user", () => {
 
     cy.get(".nav-1 > .level0").should("have.class", "level0").click();
     cy.get("#search").click().type("IPHONE").clear().type("IPHONE 6");
-
     cy.get("#search_autocomplete > ul").first().click();
+    cy.get("#product-collection-image-2").click();
+    cy.get("a").contains("Add to Wishlist").click();
+    cy.get("span")
+      .contains(
+        "IPhone has been added to your wishlist. Click here to continue shopping"
+      )
+      .and(
+        "have.text",
+        "IPhone has been added to your wishlist. Click here to continue shopping."
+      );
 
+    cy.get("#header > div > div.skip-links > div > a")
+      .should("have.class", "skip-link skip-account")
+      .click();
+    cy.get("#header-account > div > ul > li.last > a").click();
+  });
+
+  it("Test to purchase items", () => {
+    cy.get("#header > div > div.skip-links > div > a")
+      .should("have.class", "skip-link skip-account")
+      .click();
+    cy.get("#header-account > div > ul > li.last > a")
+      .should("contain", "Log In")
+      .click();
+    cy.get("#email")
+      .should("have.id", "email")
+      .type(email)
+      .should("have.value", email);
+    cy.get("#pass")
+      .should("have.class", "input-text required-entry validate-password")
+      .type(password)
+      .should("have.value", password);
+    cy.get("#send2").should("contain", "Login").click();
+    cy.get(".nav-1 > .level0").should("have.class", "level0").click();
+    cy.get("#search").click().type("IPHONE").clear().type("IPHONE 6");
+    cy.get("#search_autocomplete > ul").first().click();
     cy.get(":nth-child(2) > .product-info > .actions > .button").click();
-
     cy.get("li > span").should(
       "have.text",
       "IPhone was added to your shopping cart."
     );
     cy.get(".a-center > .btn-remove").click();
     cy.get("h1").should("have.text", "Shopping Cart is Empty");
-
-    cy.get("#header > div > div.skip-links > div > a")
-      .should("have.class", "skip-link skip-account")
-      .click();
-    cy.get("#header-account > div > ul > li.last > a").click();
-  });
-
-  it("Test cypress to finalize a purchase", () => {
-    cy.get("#header > div > div.skip-links > div > a")
-      .should("have.class", "skip-link skip-account")
-      .click();
-    cy.get("#header-account > div > ul > li.last > a")
-      .should("contain", "Log In")
-      .click();
-    cy.get("#email")
-      .should("have.id", "email")
-      .type(email)
-      .should("have.value", email);
-    cy.get("#pass")
-      .should("have.class", "input-text required-entry validate-password")
-      .type(password)
-      .should("have.value", password);
-    cy.get("#send2").should("contain", "Login").click();
-
     cy.get(".nav-1 > .level0").should("have.class", "level0").click();
     cy.get("#search").click().type("IPHONE").clear().type("IPHONE 6");
-
     cy.get("#search_autocomplete > ul").first().click();
-
     cy.get(":nth-child(2) > .product-info > .actions > .button").click();
-
     cy.get("li > span").should(
       "have.text",
       "IPhone was added to your shopping cart."
@@ -152,7 +213,7 @@ describe("Test funtionality for registred user", () => {
     cy.get("#header-account > div > ul > li.last > a").click();
   });
 
-  it("Test cypress to finalize a purchase", () => {
+  it("Test cypress to check order status", () => {
     cy.get("#header > div > div.skip-links > div > a")
       .should("have.class", "skip-link skip-account")
       .click();
