2abb199f4e35d2464d2c4b676ea287ba838ad29b
vladik9 <v.cornici@gmail.com>
2021-08-19 09:09:59 +0300

    chage api call for get/post

--- cypress/integration/Task5.spec.js
+++ cypress/integration/Task5.spec.js
@@ -7,30 +7,39 @@ describe("Test REST api", () => {
       "https://petstore.swagger.io/v2/pet/findByStatus?status=sold"
     ).then((response) => {
       const name = response.body[0].name;
+      expect(response.status).to.eq(200);
       alert(`The name was: ${name}`);
     });
   });
 
   it("Test a POST request", () => {
-    cy.request("POST", "https://petstore.swagger.io/v2/store/order", {
+    const jsBody = {
       id: 5,
       petId: 2,
       quantity: 1,
       shipDate: "2021-08-20T19:00:00.000Z",
       status: "placed",
       complete: true,
-    }).then((response) => {
+    };
+
+    cy.request(
+      "POST",
+      "https://petstore.swagger.io/v2/store/order",
+      jsBody
+    ).then((response) => {
       console.log(response.body);
       if (response.body.status === "placed") alert("The order is placed!");
+      expect(response.status).to.eq(200);
     });
   });
 
   it("Test a Login GET request", () => {
-    cy.request(
-      "GET",
-      " https://petstore.swagger.io/v2/user/login?username=Ann&password=1234"
-    ).then((response) => {
+    const userName = "admin_test";
+    const userPassword = "password1234";
+    const url = `https://petstore.swagger.io/v2/user/login?username=${userName}&password=${userPassword}`;
+    cy.request("GET", url).then((response) => {
       if (response.status === 200) alert("The user has successfully login!");
+      expect(response.status).to.eq(200);
     });
   });
 });
