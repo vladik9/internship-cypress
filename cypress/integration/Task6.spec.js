c2ebbb9c3cb97f4e4fd07606df008657cf52f897
vladik9 <v.cornici@gmail.com>
2021-08-20 10:21:16 +0300

    adding task6 and ignore todo

--- cypress/integration/Task6.spec.js
+++ cypress/integration/Task6.spec.js
@@ -1,5 +1,7 @@
-describe("", () => {
-  beforeEach(() => {});
+describe("POM model in Cypress", () => {
+  beforeEach(() => {
+    cy.visit("http://live.demoguru99.com");
+  });
 
   it("Test a GET request", () => {});
 });
