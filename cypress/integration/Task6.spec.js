4c3b4906c23447e575ac03cf6dfd951c696e1666
vladik9 <v.cornici@gmail.com>
2021-08-20 11:25:40 +0300

    finish tast 6

--- cypress/integration/Task6.spec.js
+++ cypress/integration/Task6.spec.js
@@ -1,7 +1,92 @@
+var password = "amParolaCea1MaiTare";
+var email = "canomeI105@email.com";
+const pom = new POM();
+const genMeEmailcode = function () {
+  const randomNum1 =
+    Math.floor(Math.random() * 2) + Math.floor(Math.random() * 3) + 5;
+  const randomNum2 =
+    Math.floor(Math.random() * 7) + Math.floor(Math.random() * 2) + 4;
+  const randomNum3 =
+    Math.floor(Math.random() * 3) + Math.floor(Math.random() * 3) + 2;
+  const randomNum4 =
+    Math.floor(Math.random() * 8) + Math.floor(Math.random() * 0) + 2;
+  return (email = `ca${randomNum4}nome${randomNum3}I${randomNum1}${randomNum2}@email.com`);
+};
+
+import POM from "./cypressTask6POMFile.spec.js";
 describe("POM model in Cypress", () => {
   beforeEach(() => {
     cy.visit("http://live.demoguru99.com");
   });
 
-  it("Test a GET request", () => {});
+  it("Register POM model", () => {
+    genMeEmailcode();
+    cy.get(
+      pom
+        .getAccountButton()
+        .should("have.class", "skip-link skip-account")
+        .click()
+    );
+    cy.get(pom.getRegisterButton().should("contain", "Register").click());
+    cy.get(pom.getFirstName().should("have.id", "firstname").type("Canome"));
+
+    cy.get(pom.getMiddleName().should("have.id", "middlename").type("R"));
+
+    cy.get(pom.getLastName().should("have.id", "lastname").type("Eduard"));
+    cy.get(pom.getEmail().should("have.id", "email_address").type(email));
+    cy.get(
+      pom
+        .getPassword()
+        .should("have.class", "input-text required-entry validate-password")
+        .type(password)
+    );
+    cy.get(
+      pom.getConfirmPassword().should("have.id", "confirmation").type(password)
+    );
+    cy.get(pom.getSubscriptionNewsletter().check().should("be.checked"));
+    cy.get(
+      pom.getSumbitRegistrationButton().should("have.class", "button").click()
+    );
+    cy.get(
+      pom
+        .getOkMessageRegistrationButton()
+        .should(
+          "have.text",
+          "Thank you for registering with Main Website Store."
+        )
+    );
+    cy.get(pom.getAccountButton().click());
+    cy.get(pom.getLogOutButton().click());
+  });
+
+  it("LogIn POM model", () => {
+    cy.get(
+      pom
+        .getAccountButton()
+        .should("have.class", "skip-link skip-account")
+        .click()
+    );
+    cy.get(pom.getLogOutButton().should("contain", "Log In").click());
+    cy.get(
+      pom
+        .getEmaillogIn()
+        .should("have.id", "email")
+        .type(email)
+        .should("have.value", email)
+    );
+    cy.get(
+      pom
+        .getEmailLogIn()
+        .should("have.class", "input-text required-entry validate-password")
+        .type(password)
+        .should("have.value", password)
+    );
+    cy.get(pom.getLogInButton().should("contain", "Login").click());
+    cy.get(pom.getLogInSuccessMessage().should("have.text", "My Dashboard"));
+    cy.get(pom.getAccountButton().click());
+    cy.get(pom.getLogOutButton().click());
+    cy.get(
+      pom.getLogOutMessage().should("have.text", "You are now logged out")
+    );
+  });
 });
--- cypress/integration/cypressTask6POMFile.spec.js
+++ cypress/integration/cypressTask6POMFile.spec.js
@@ -1,1 +1,75 @@
-module.exports = function () {};
+var password = "amParolaCea1MaiTare";
+var email = "canomeI105@email.com";
+const genMeEmailcode = function () {
+  const randomNum1 =
+    Math.floor(Math.random() * 2) + Math.floor(Math.random() * 3) + 5;
+  const randomNum2 =
+    Math.floor(Math.random() * 7) + Math.floor(Math.random() * 2) + 4;
+  const randomNum3 =
+    Math.floor(Math.random() * 3) + Math.floor(Math.random() * 3) + 2;
+  const randomNum4 =
+    Math.floor(Math.random() * 8) + Math.floor(Math.random() * 0) + 2;
+  return (email = `ca${randomNum4}nome${randomNum3}I${randomNum1}${randomNum2}`);
+};
+
+// for registering
+class UtilsButtons {
+  getAccountButton() {
+    return cy.get("#header > div > div.skip-links > div > a");
+  }
+  getRegisterButton() {
+    return cy.get("#header-account > div > ul > li:nth-child(5) > a");
+  }
+  getFirstName() {
+    return cy.get("#firstname");
+  }
+  getMiddleName() {
+    return cy.get("#middlename");
+  }
+  getLastName() {
+    return cy.get("#lastname");
+  }
+  getEmail() {
+    return cy.get("#email_address");
+  }
+  getPassword() {
+    return cy.get("#password");
+  }
+  getConfirmPassword() {
+    return cy.get("#confirmation");
+  }
+
+  getSubscriptionNewsletter() {
+    return cy.get("#is_subscribed");
+  }
+  getSumbitRegistrationButton() {
+    return cy.get("#form-validate > div.buttons-set > button");
+  }
+  getOkMessageRegistrationButton() {
+    return cy.get("li > span");
+  }
+  getLogOutButton() {
+    return cy.get("#header-account > div > ul > li.last > a");
+  }
+  getLogInButton() {
+    return cy.get("#header-account > div > ul > li.last > ");
+  }
+  getEmaillogIn() {
+    return cy.get("#email");
+  }
+  getEmailLogIn() {
+    return cy.get("#pass");
+  }
+  getLogInButton() {
+    return cy.get("#send2");
+  }
+  getLogInSuccessMessage() {
+    return cy.get(
+      "body > div > div > div.main-container.col2-left-layout > div > div.col-main > div > div > div.page-title > h1"
+    );
+  }
+  getLogOutMessage() {
+    return cy.get("h1").contains("You are now logged out");
+  }
+}
+export default UtilsButtons;
